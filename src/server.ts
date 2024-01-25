6a0214f6da2e8af2cebad75fed0cb0442c910ce6
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2024-01-25 15:36:29 +0000

    Added serverless package and used sls command to setupAWS cloudformation to deploy Lambda function to AWS env.

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,15 @@ import 'dotenv/config'
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { schema } from "./schema/schema.js";
+import { handlers, startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 
 const server = new ApolloServer({
   schema: schema,
 });
 
-const { url } = await startStandaloneServer(server);
+// const { url } = await startStandaloneServer(server);
+// console.log(`🚀  Server ready at ${url}`);
 
-console.log(`🚀  Server ready at ${url}`);
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server, handlers.createAPIGatewayProxyEventV2RequestHandler()
+);
